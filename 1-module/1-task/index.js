d481d2d61f5f88e353762112b2e1dfd2a96b7aba
f2mars <norkuse@gmail.com>
2022-07-15 16:32:57 +0300

    make function to calculate factorial

--- 1-module/1-task/index.js
+++ 1-module/1-task/index.js
@@ -1,3 +1,11 @@
 function factorial(n) {
-  // ваш код...
+  if (n === 0 || n === 1) {
+    return 1;
+  }
+
+  let result = 2;
+  for (let i = 3; i <= n; i++) {
+    result *= i;
+  }
+  return result;
 }
